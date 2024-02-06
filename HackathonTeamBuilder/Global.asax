0dd3eefe9a2ae86bf7e8301eb3a85974fea88fb3
Lap Wang Wong <leonwonglww@gmail.com>
2024-02-06 14:57:16 -0500

    Add project files.

---
+++ HackathonTeamBuilder/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HackathonTeamBuilder.MvcApplication" Language="C#" %>
---
+++ HackathonTeamBuilder/Migrations/202402061943365_add-new-fields-User-model.cs
@@ -0,0 +1,28 @@
+﻿namespace HackathonTeamBuilder.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class addnewfieldsUsermodel : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.AspNetUsers", "Bio", c => c.String());
+            AddColumn("dbo.AspNetUsers", "FullName", c => c.String());
+            AddColumn("dbo.AspNetUsers", "LinkedinUrl", c => c.String());
+            AddColumn("dbo.AspNetUsers", "GithubUrl", c => c.String());
+            AddColumn("dbo.AspNetUsers", "PortfolioUrl", c => c.String());
+            AddColumn("dbo.AspNetUsers", "Role", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.AspNetUsers", "Role");
+            DropColumn("dbo.AspNetUsers", "PortfolioUrl");
+            DropColumn("dbo.AspNetUsers", "GithubUrl");
+            DropColumn("dbo.AspNetUsers", "LinkedinUrl");
+            DropColumn("dbo.AspNetUsers", "FullName");
+            DropColumn("dbo.AspNetUsers", "Bio");
+        }
+    }
+}
