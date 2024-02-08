0f3ec9edb76fae1530104b6962a4ff806aba0294
Lap Wang Wong <leonwonglww@gmail.com>
2024-02-08 00:25:54 -0500

    feat: added team listing page

---
+++ HackathonTeamBuilder/Controllers/TeamController.cs
@@ -0,0 +1,32 @@
+﻿using HackathonTeamBuilder.Models;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+
+namespace HackathonTeamBuilder.Controllers
+{
+    public class TeamController : Controller
+    {
+
+        HttpClient client;
+        JavaScriptSerializer jss;
+        public TeamController()
+        {
+            client = new HttpClient();
+            client.BaseAddress = new System.Uri(Constant.BASE_URL);
+        }
+        /// <summary>
+        /// Display a list of teams for a hackthon
+        /// </summary>
+        /// <param name="id">hackthon id</param>
+        /// <returns></returns>
+        public ActionResult List(int Id)
+        {
+            HttpResponseMessage response = client.GetAsync($"teamdata/ListTeamsByHackathon/{Id}").Result;
+            List<Team> teams = response.Content.ReadAsAsync<List<Team>>().Result;
+
+            return View(teams);
+        }
+    }
+}
---
+++ HackathonTeamBuilder/Controllers/TeamDataController.cs
@@ -0,0 +1,150 @@
+﻿using HackathonTeamBuilder.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace HackathonTeamBuilder.Controllers
+{
+    public class TeamDataController : ApiController
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: /teamdata/list
+        [HttpGet]
+        public IHttpActionResult List()
+        {
+            try
+            {
+                List<Team> teams = db.Teams.ToList();
+                return Ok(teams);
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+        }
+
+        // GET: /teamdata/find/{id}
+        [HttpGet]
+        public IHttpActionResult Find(int id)
+        {
+            try
+            {
+                Team team = db.Teams.Find(id);
+
+                if (team == null)
+                {
+                    return NotFound();
+                }
+
+                return Ok(team);
+            }
+            catch (Exception ex)
+            {
+
+                return InternalServerError(ex);
+            }
+        }
+
+        /// <summary>
+        /// Get a list of teams by hackathon id
+        /// </summary>
+        /// <param name="id">hackathon id</param>
+        /// <returns></returns>
+        // GET: /teamdata/ListTeamsByHackathon/{id}
+        [HttpGet]
+        public IHttpActionResult ListTeamsByHackathon(int id)
+        {
+            try
+            {
+                List<Team> teams = db.Teams.Where(t => t.HackathonId == id).ToList();
+
+                return Ok(teams);
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+        }
+
+
+        // POST: /teamdata/create
+        [HttpPost]
+        public IHttpActionResult Create(Team team)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+
+                db.Teams.Add(team);
+                db.SaveChanges();
+
+                return CreatedAtRoute("DefaultApi", new { id = team.Id }, team);
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+        }
+
+        // PUT: /teamdata/update/{id}
+        [HttpPut]
+        public IHttpActionResult Update(int id, Team updatedTeam)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+
+                Team existingTeam = db.Teams.Find(id);
+
+                if (existingTeam == null)
+                {
+                    return NotFound();
+                }
+
+                existingTeam.TeamLeaderId = updatedTeam.TeamLeaderId;
+                existingTeam.Requirements = updatedTeam.Requirements;
+                existingTeam.MaxNumOfMembers = updatedTeam.MaxNumOfMembers;
+
+                db.SaveChanges();
+
+                return Ok(existingTeam);
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+        }
+
+        // DELETE: /teamdata/delete/{id}
+        [HttpDelete]
+        public IHttpActionResult Delete(int id)
+        {
+            try
+            {
+                Team team = db.Teams.Find(id);
+
+                if (team == null)
+                {
+                    return NotFound();
+                }
+
+                db.Teams.Remove(team);
+                db.SaveChanges();
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+        }
+    }
+}
